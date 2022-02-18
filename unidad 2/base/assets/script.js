5566080e242ed3456974eac737ec71cf584e0adf
rossy1668 <97636042+rossy1668@users.noreply.github.com>
2022-02-18 15:57:52 -0500

    Add files via upload

---
+++ unidad 2/base/assets/script.js
@@ -0,0 +1,6 @@
+
+      var bodyCacheable = true;
+      
+      var exclusionReason = {"shouldRender":true,"forced":false};
+      var ssrInfo = {"renderBodyTime":509,"renderTimeStamp":1638475351565}
+  
